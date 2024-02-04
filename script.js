5846e08d1180805fb65c91902d4c8db69e833eb1
GultekinEnes <135526422+GultekinEnes@users.noreply.github.com>
2024-02-04 11:36:47 +0300

    Kaynak kodlar eklendi

---
+++ script.js
@@ -0,0 +1,166 @@
+let puanText = document.getElementById('puanText');
+puan = 0;
+
+puanText.innerHTML = "0"
+
+const kartTemplate = `
+    <div class="kart-cerceve">
+        <div class="kart-onyuz">
+            <img src="https://via.placeholder.com/100x100?text=?">
+        </div>
+
+        <div class="kart-arkayuz">
+            <img src="">
+        </div>
+    </div>
+`;
+
+let randomNum = function(){
+    let randomArray = [];
+    for (let i = 0; i < 8; i++){
+       let randomNumbers = Math.floor(Math.random() * 99);
+        randomArray.push(randomNumbers, randomNumbers);
+        if (randomArray.length > 6) break;
+    }
+    return randomArray
+};
+
+
+/*
+Görev 2: Bu numaraları 1-99 arası(1 ve 99 dahil) sayılardan rastgele 4 çift oluşturacak şekilde üreten bir fonksiyon yazarak, kod bloğundaki array değerini bu fonksiyondan dönen array ile değiştiren kodları yazın
+*/
+
+function rastgeleNumara() {
+    var num_list = [];
+    //rastgele numaralr sayesinde yeni dizi oluşturcak ve her seferinde resimler değişicek
+    for (let i = 0; i < 4; i++) {
+        let sayi = Math.floor(Math.random() * 99) + 1;
+
+        if (num_list.includes(sayi)) {
+            i--;
+            continue;
+        }
+
+        num_list.push(sayi, sayi);
+
+        if (num_list.length >= 8) {
+            break;
+        }
+    }
+
+    num_list.sort(() => 0.5 - Math.random()); // resimleri rastgele sıralıcak
+
+    return num_list;
+}
+
+
+const fotoNumaralari = rastgeleNumara();
+
+
+console.log(fotoNumaralari);
+
+for (fotoNumara of fotoNumaralari) {
+    const yenikart = document.createElement("div");
+    yenikart.innerHTML = kartTemplate;
+    yenikart.classList.add("kart");
+    yenikart.querySelector(".kart-arkayuz img").src = `https://lipsum.app/id/${fotoNumara}/100x100`;
+    document.querySelector("div#oyun-cerceve").append(yenikart);
+
+    //Her bir karta tıklandığında "kartTiklama" fonksiyonu çalışacak.
+    yenikart.addEventListener("click", kartTiklama);
+}
+
+function kartTiklama(olay) {
+    //Tıklanan kartı seçilen olarak değişkene atayalım
+    const secilenKart = olay.currentTarget;
+
+    //Tıklanan kart eslesti classına sahipse daha önce başka kartla eşleşmiş ve zaten açık durumda demektir, işlem yapmayacağız.
+    if (secilenKart.classList.contains("eslesti") === true) {
+        return;
+    }
+
+    //Tıklanan ve açılan karta tekrar tıklanırsa işlem yapmayacağız.
+    if (secilenKart.classList.contains("acik") === true) {
+        return;
+    }
+
+    //Peşpeşe kartlara tıklandığında 2'den fazla kart tıklanmasını engellememiz gerekiyor.
+    const tumAcikKartlar = document.querySelectorAll(".acik");
+    if (tumAcikKartlar.length === 2) {
+        return;
+    }
+
+    //Açık olan kart varsa seçelim.
+    const acikKart = document.querySelector(".acik");
+
+    //Hiç açık kart yoksa tıklanan karta açık class veriyoruz ve fonksiyondan çıkıyoruz.
+    if (acikKart === null) {
+        secilenKart.classList.add("acik");
+
+        setTimeout(
+            () => {
+                secilenKart.classList.remove("acik");
+            }, 1500
+        );
+        return;
+    }
+
+    //Daha önce bir açık kartımız varmış, son seçilen karta da açık class vererek tersini çevirelim.
+    secilenKart.classList.add("acik");
+
+    //Açık kartlara ait img etiketlerinin src görsel dosyaları eşleşiyor mu?
+    const acikKartImg = acikKart.querySelector(".kart-arkayuz img");
+    const secilenKartImg = secilenKart.querySelector(".kart-arkayuz img");
+
+    if (acikKartImg.src === secilenKartImg.src) {
+      //İki açık kart arasında eşleşme var.
+      acikKart.classList.add("eslesti");
+      secilenKart.classList.add("eslesti");
+
+      puan++;
+
+      console.log(puan);
+
+      puanText.innerHTML = puan;
+
+      /*
+            Görev 1: Kullanıcı 4 kartı da eşleştirdiğinde sayfa ortasında beliren hareketli gif dosyası formatında bir kutlama görseli belirsin ve bu fotoğraf 5 saniye sonra ortadan kaybolsun.
+        */
+
+
+        //Kutlama Gifi
+        function kutlama() {
+            const kutlamaGif = document.createElement("img");
+            kutlamaGif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNW05N3UwMTFqdnIxMzV5ZHVkc215ZW9xbjBzc2xlNW5rM3F4eHlmYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/blSTtZehjAZ8I/giphy.gif"
+            document.body.appendChild(kutlamaGif);
+            setTimeout(() => {
+                document.body.removeChild(kutlamaGif);
+            }, 5000);
+            kutlamaGif.style.position = "absolute";
+            kutlamaGif.style.left = "50%";
+            kutlamaGif.style.top = "50%";
+            kutlamaGif.style.transform = "translate(-50%, -50%)";
+            kutlamaGif.style.width = "650px";
+            kutlamaGif.style.height = "650px";
+        }
+        
+        if (puan === 4) {
+            kutlama();
+        }
+        
+
+      acikKart.classList.remove("acik");
+      secilenKart.classList.remove("acik");
+
+      setTimeout(() => {
+        acikKart.removeEventListener("click", kartTiklama);
+        secilenKart.removeEventListener("click", kartTiklama);
+      }, 1000);
+    } else {
+        //İki açık kartın görsel dosya adı birbirinden farklı, eşleşme yok, kartlar kapansın.
+        setTimeout(() => {
+            acikKart.classList.remove("acik");
+            secilenKart.classList.remove("acik");
+        }, 1500);
+    }
+}
